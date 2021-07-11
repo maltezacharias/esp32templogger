fd70118d38fe508d3aa236578bcfffdd606b1ae2
Malte Zacharias <me@malte-zacharias.de>
2021-07-11 11:31:27 +0200

    Make Sensor Sample Period configurable

--- main/sensorTask.h
+++ main/sensorTask.h
@@ -5,7 +5,7 @@
 #define GPIO_DS18B20_0       (CONFIG_ONE_WIRE_GPIO)
 #define MAX_DEVICES          (CONFIG_MAX_SENSORS)
 #define DS18B20_RESOLUTION   (DS18B20_RESOLUTION_12_BIT)
-#define SAMPLE_PERIOD        (1000)   // milliseconds
+#define SAMPLE_PERIOD        (CONFIG_TEMPERATURE_SAMPLE_PERIOD)   // milliseconds
 
 extern DS18B20_Info * sensors[];
 void sensorTask(void *pvParameter);
