ce9d0904ad03144fb970c04bdd6f8aa496f0a743
Malte Zacharias <me@malte-zacharias.de>
2021-07-11 11:33:14 +0200

    Start implementation mqttTask

---
+++ main/mqttTask.c
@@ -0,0 +1,9 @@
+#include "mqtt_client.h"
+#include "mqttTask.h"
+
+esp_mqtt_client_handle_t mqttClient;
+esp_mqtt_client_config_t mqttConfig;
+
+void mqttTask(void * taskParameters) {
+    
+}
---
+++ main/mqttTask.h
@@ -0,0 +1,1 @@
+void mqttTask(void * taskParameters);
